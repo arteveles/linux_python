c8c098e1584d329d44be5e8f9324dcf8fd08e7c2
veles <a9131038090@gmail.com>
2022-11-29 17:51:39 +0300

    refactor: 1) standart stdout 2) round float till 1 number 3) return full name of process

--- home_work/parser.py
+++ home_work/parser.py
@@ -1,6 +1,7 @@
 from subprocess import run, PIPE
 import datetime
 import pandas as pd
+from pathlib import Path
 
 res = run(['ps', 'aux'], stdout=PIPE)
 procs = res.stdout.decode().split('\n')
@@ -66,26 +67,35 @@ for p in procs[1:]:
             count_mem_proc = chunks[titles.index('%MEM')]
             count_mem.append(count_mem_proc)
             sum_m = pd.Series(count_mem, dtype="float64").sum()
+            sum_m_format = round(sum_m, 1)
 
         if proc_cpu >= max_cpu or proc_cpu <= max_cpu:
             max_cpu_1 = proc_cpu
             max_cpu_proc_1 = chunks[titles.index('%CPU')]
             count_cpu.append(max_cpu_proc_1)
             sum_c = pd.Series(count_cpu, dtype="float64").sum()
+            sum_c_format = round(sum_c, 1)
 
 
 def now_time():
     return datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 
+dict_report = {
+    f"Пользователей системы: {unique_use}",
+    f"Процессов запущено: {result_count}",
+    f"Пользовательских процессов: {dictionary}",
+    f"Всего MEM используется: {sum_m_format}",
+    f"Всего CPU используется: {sum_c_format}",
+    f"Больше всего MEM использует: {max_mem} {Path(max_mem_proc).name[:20]}",
+    f"Больше всего CPU использует: {max_cpu} {Path(max_cpu_proc).name[:20]}"
+}
+result = '\n'.join(dict_report)
+
+"""Standart output in CLI"""
+print(result)
+
+"""Create and write fo file"""
 with open(f"{now_time()}.txt", "w+") as file:
-    file.write(
-        f"Пользователи системы: {unique_use}"
-        f"\nПроцессов запущено: {result_count}"
-        f"\nПользовательских процессов: {dictionary}"
-        f"\nВсего MEM используется, %: {sum_m}"
-        f"\nВсего CPU используется, %: {sum_c}"
-        f"\nБольше всего MEM использует: {max_mem} ({max_mem_proc[:20]})"
-        f'\nБольше всего CPU использует:  {max_cpu} ({max_cpu_proc[:20]})'
-    )
+    file.write(result)
     file.close()
